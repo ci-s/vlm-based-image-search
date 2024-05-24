f8ce1dabd084971b2f9491b47dc96f2cd5bec392
cisem <cisemaltan@gmail.com>
2024-05-24 16:41:08 +0200

    feat: coco and similarity utils

---
+++ src/data/coco.py
@@ -0,0 +1,14 @@
+import json
+
+captions_path = "/usr/prakt/s0070/vlm-based-image-search/data/coco/annotations/captions_val2017.json"
+# instances_path = "/usr/prakt/s0070/vlm-based-image-search/data/coco/annotations/instances_val2017.json"
+
+captions = json.load(open(captions_path))
+# instances = json.load(open(instances_path))
+
+filename_id_dict = {image["file_name"]: image["id"] for image in captions["images"]}
+id_caption_dict = {image["image_id"]: image["caption"] for image in captions["annotations"]}
+
+def get_caption(image_filename):
+    image_id = filename_id_dict[image_filename]
+    return id_caption_dict[image_id]
